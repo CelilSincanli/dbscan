00f4b41340290ffee43e953d6e07cd3c5d269972
CelilSincali <icelilsincanli@gmail.com>
2024-05-26 23:59:45 +0300

    feat: added point cloud struct

--- include/dbscan.hpp
+++ include/dbscan.hpp
@@ -3,8 +3,36 @@
 
 #include <vector>
 
-class DBSCAN {
+#include <cassert>
+#include <cstddef>
+#include <span>
+#include <vector>
+#include <cstdlib>
 
+class DBSCAN {
+public:
+    struct PointCloud {
+        struct Point {
+            float x, y, z;
+        };
+
+        using coord_t = float; //!< The type of each coordinate
+
+        std::vector<Point> pts;
+
+        // Must return the number of data points
+        size_t kdtree_get_point_count() const;
+
+        // Returns the dim'th component of the idx'th point in the class
+        float kdtree_get_pt(const size_t idx, const size_t dim) const;
+
+        // Optional bounding-box computation
+        template <class BBOX>
+        bool kdtree_get_bbox(BBOX& /* bb */) const;
+    };
+
+    void dbscan(PointCloud& cloud, float eps, int min_pts);
 };
+
 
 #endif // DBSCAN_HPP
--- src/dbscan.cpp
+++ src/dbscan.cpp
@@ -2,6 +2,33 @@
 #include "dbscan.hpp" // Adjust this to the correct path if needed
 #include "nanoflann.hpp" // Adjust this to the correct path if needed
 
-int main(int argc, char* argv[]) {
 
+
+size_t DBSCAN::PointCloud::kdtree_get_point_count() const {
+    return pts.size();
+}
+
+float DBSCAN::PointCloud::kdtree_get_pt(const size_t idx, const size_t dim) const {
+    if (dim == 0)
+        return pts[idx].x;
+    else if (dim == 1)
+        return pts[idx].y;
+    else
+        return pts[idx].z;
+}
+
+template <class BBOX>
+bool DBSCAN::PointCloud::kdtree_get_bbox(BBOX& /* bb */) const {
+    return false;
+}
+
+void DBSCAN::dbscan(PointCloud& cloud, float eps, int min_pts) {
+
+    std::cout << "Number of points: " << cloud.kdtree_get_point_count() << std::endl;
+}
+
+
+int main() {
+
+    return 0;
 }

0b608551c0d24ce777967beb888b30c27b7b61ba
CelilSincali <icelilsincanli@gmail.com>
2024-05-26 21:56:51 +0300

    fisrt commit

---
+++ include/dbscan.hpp
@@ -0,0 +1,10 @@
+#ifndef DBSCAN_HPP
+#define DBSCAN_HPP
+
+#include <vector>
+
+class DBSCAN {
+
+};
+
+#endif // DBSCAN_HPP
---
+++ src/dbscan.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "dbscan.hpp" // Adjust this to the correct path if needed
+#include "nanoflann.hpp" // Adjust this to the correct path if needed
+
+int main(int argc, char* argv[]) {
+
+}
